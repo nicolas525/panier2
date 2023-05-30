91861e925f6b79c27c4038ca275faf5f12354e18
nicepowpowloko <90335368+nicepowpowloko@users.noreply.github.com>
2023-05-30 13:59:43 +0200

    30/05

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
- session_start() ;
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,29 +11,31 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-   
-    <a href="panier.php" class="link">Panier<span><?=array_sum($_SESSION['panier'])?></span></a>
+    <a href="panier.php" class="link">Panier<span><?= isset($_SESSION['panier']) ? array_sum($_SESSION['panier']) : 0 ?></span></a>
     <section class="products_list">
         <?php 
-        
         include_once "con_dbb.php";
-       
-         $req = mysqli_query($con, "SELECT * FROM products");
-         while($row = mysqli_fetch_assoc($req)){ 
+        $req = mysqli_query($con, "SELECT * FROM products");
+
+        if ($req && mysqli_num_rows($req) > 0) {
+            while ($row = mysqli_fetch_assoc($req)) {
         ?>
         <form action="" class="product">
             <div class="image_product">
-                <img src="project_images/<?=$row['img']?>">
+                <img src="project_images/<?= $row['img'] ?>" alt="Image du produit">
             </div>
             <div class="content">
-                <h4 class="noms"><?=$row['noms']?></h4>
-                <h2 class="prix"><?=$row['prix']?>€</h2>
-                <a href="ajouter_panier.php?id=<?=$row['id']?>" class="id_product">Ajouter au panier</a>
+                <h4 class="noms"><?= $row['noms'] ?></h4>
+                <h2 class="prix"><?= $row['prix'] ?>€</h2>
+                <a href="ajouter_panier.php?id=<?= $row['id'] ?>" class="id_product">Ajouter au panier</a>
             </div>
         </form>
-
-        <?php } ?>
-       
+        <?php 
+            }
+        } else {
+            echo "Aucun produit trouvé.";
+        }
+        ?>
     </section>
 </body>
 </html>
--- panier.php
+++ panier.php
@@ -1,15 +1,19 @@
-<?php 
-   session_start();
-   include_once "con_dbb.php";
+<?php
+session_start();
+include_once "con_dbb.php";
 
-   //supprimer les produits
-   //si la variable del existe
-   if(isset($_GET['del'])){
-    $id_del = $_GET['del'] ;
-    //suppression
-    unset($_SESSION['panier'][$id_del]);
-   }
+// Supprimer les produits
+// Vérifier si la variable 'del' existe dans la requête GET
+if (isset($_GET['del'])) {
+    $id_del = $_GET['del'];
+
+    // Vérifier si l'élément existe dans le panier
+    if (isset($_SESSION['panier'][$id_del])) {
+        unset($_SESSION['panier'][$id_del]);
+    }
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -30,35 +34,37 @@
                 <th>Quantité</th>
                 <th>Action</th>
             </tr>
-            <?php 
-              $total = 0 ;
-              
-              
-              $ids = array_keys($_SESSION['panier']);
-              
-              if(empty($ids)){
-                echo "Votre panier est vide";
-              }else {
-                //si oui 
-                $products = mysqli_query($con, "SELECT * FROM products WHERE id IN (".implode(',', $ids).")");
+            <?php
+            $total = 0;
 
-                
-                foreach($products as $product):
-                    
-                    $total = $total + $product['prix'] * $_SESSION['panier'][$product['id']] ;
-                ?>
-                <tr>
-                    <td><img src="project_images/<?=$product['img']?>"></td>
-                    <td><?=$product['noms']?></td>
-                    <td><?=$product['prix']?>€</td>
-                    <td><?=$_SESSION['panier'][$product['id']] ?></td>
-                    <td><a href="panier.php?del=<?=$product['id']?>"><img src="delete.png"></a></td>
-                </tr>
+            // Vérifier si le panier est vide
+            if (empty($_SESSION['panier'])) {
+                echo "<tr><td colspan='5'>Votre panier est vide</td></tr>";
+            } else {
+                $ids = array_keys($_SESSION['panier']);
 
-            <?php endforeach ;} ?>
+                // Récupérer les informations des produits à partir de la base de données
+                $product_ids = implode(',', $ids);
+                $products = mysqli_query($con, "SELECT * FROM products WHERE id IN ($product_ids)");
+
+                foreach ($products as $product):
+                    $quantity = $_SESSION['panier'][$product['id']];
+                    $subtotal = $product['prix'] * $quantity;
+                    $total += $subtotal;
+                    ?>
+                    <tr>
+                        <td><img src="project_images/<?= $product['img'] ?>" alt="Image du produit"></td>
+                        <td><?= $product['noms'] ?></td>
+                        <td><?= $product['prix'] ?>€</td>
+                        <td><?= $quantity ?></td>
+                        <td><a href="panier.php?del=<?= $product['id'] ?>"><img src="delete.png" alt="Supprimer"></a></td>
+                    </tr>
+                <?php endforeach;
+            }
+            ?>
 
             <tr class="total">
-                <th>Total : <?=$total?>€</th>
+                <th>Total : <?= $total ?>€</th>
             </tr>
         </table>
     </section>
